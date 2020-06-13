153091f519d76452e725cc542f54aafb33a66478
ilkinpashayev <pashayev.ilkin@gmail.com>
2020-06-14 03:12:54 +0400

    Created:
     Upload controller
     Upload view
    Added validation for Email
    Added validation for File

---
+++ AWSUploadFile/Controllers/UploadController.cs
@@ -0,0 +1,30 @@
+﻿using AWSUploadFile.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AWSUploadFile.Controllers
+{
+    public class UploadController : Controller
+    {
+        // GET: Upload
+        public ActionResult Upload()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        
+        public void UploadFile(uploadFormObject uploadFormObj)
+        {
+            if (!ModelState.IsValid)
+            {
+                
+            }
+
+            
+        }
+    }
+}
---
+++ AWSUploadFile/Domain/uploadFormObject.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AWSUploadFile.Domain
+{
+    public class uploadFormObject
+    {
+        [RegularExpression(@"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$")]
+
+        public string Email { get; set; }
+
+        [Required]
+        public HttpPostedFileBase file { get; set; }
+    }
+}
